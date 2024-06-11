a41a0a9a927309c7942660a1ba595e4ea06df1e7
lakko <lakko@LAPTOP-TSGU3JAG.attlocal.net>
2024-06-11 17:45:49 -0400

    update the code for Poll SCM trigger

--- src/mypractice/welcome.java
+++ src/mypractice/welcome.java
@@ -7,6 +7,7 @@ System.out.println("hi,this is my practice");
 System.out.println("java training");
 System.out.println("89+23");System.out.println("Welcome Goutham");
 System.out.println("WELCOME TO CODE LAB TECHNOLOGY");
+System.out.println("Testing Poll SCM for the fist time");
 
 	}
 
