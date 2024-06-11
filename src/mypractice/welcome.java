d5f7a8a6feffeaedf9a3f76f1008a9365dc5b200
lakko <lakko@LAPTOP-TSGU3JAG.attlocal.net>
2024-06-11 16:42:37 -0400

    greeting goutham

--- src/mypractice/welcome.java
+++ src/mypractice/welcome.java
@@ -5,7 +5,7 @@ public class welcome {
 	public static void main(String[] args) {
 System.out.println("hi,this is my practice");	
 System.out.println("java training");
-System.out.println("89+23");
+System.out.println("89+23");System.out.println("Welcome Goutham");
 
 	}
 
