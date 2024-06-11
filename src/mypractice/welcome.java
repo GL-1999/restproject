cfe648f7df5b5101fae44af7cee7a68d57a0af59
lakko <lakko@LAPTOP-TSGU3JAG.attlocal.net>
2024-06-11 17:49:16 -0400

    offer updated for iphone

--- src/mypractice/welcome.java
+++ src/mypractice/welcome.java
@@ -9,6 +9,9 @@ System.out.println("89+23");System.out.println("Welcome Goutham");
 System.out.println("WELCOME TO CODE LAB TECHNOLOGY");
 System.out.println("Testing Poll SCM for the fist time");
 
+System.out.println("$100usd discount on iPhone 15Pro Max this festival");
+System.out.println("Hurry this offer is limited");
+
 	}
 
 }
