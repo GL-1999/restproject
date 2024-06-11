b43bc7e155901491e3548a11c5a4897b2faa4d2b
lakko <lakko@LAPTOP-TSGU3JAG.attlocal.net>
2024-06-11 16:39:11 -0400

    uploading rohith project.

---
+++ src/mypractice/welcome.java
@@ -0,0 +1,12 @@
+package mypractice;
+
+public class welcome {
+
+	public static void main(String[] args) {
+System.out.println("hi,this is my practice");	
+System.out.println("java training");
+System.out.println("89+23");
+
+	}
+
+}
