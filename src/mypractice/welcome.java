e496125be640631866ef1a67db300bf7af60d8bb
lakko <lakko@LAPTOP-TSGU3JAG.attlocal.net>
2024-06-11 20:56:50 -0400

    hurry up about offer given

--- src/mypractice/welcome.java
+++ src/mypractice/welcome.java
@@ -16,6 +16,8 @@ System.out.println("welcome to learning technology");
 System.out.println("i'm learning this for second time");
 
 System.out.println("I'M updating the poll scm for the second time");
+System.out.println("there is a 100 give away for the laptop purchase above 1800");
+System.out.println("Hurry up there is only 3 hoyrs left");
 	}
 
 }
